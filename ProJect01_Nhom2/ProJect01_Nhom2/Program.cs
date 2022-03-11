98a81d60caa44435f08e8df68e53d2a63c65c2d2
TienSultanGaylord <101380528+TienSultanGaylord@users.noreply.github.com>
2022-03-11 19:28:03 +0700

    Add files via upload

---
+++ ProJect01_Nhom2/ProJect01_Nhom2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProJect01_Nhom2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
