b64959fcdeaee02117959400cc895f18498fda62
TienSultanGaylord <101380528+TienSultanGaylord@users.noreply.github.com>
2022-03-11 20:18:29 +0700

    Update Program.cs

--- Project01_Nhom2/Project01_Nhom2/Program.cs
+++ Project01_Nhom2/Project01_Nhom2/Program.cs
@@ -7,7 +7,6 @@ namespace Project01_Nhom2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello My Name Is Nguyen Quoc Tien!");
-            Console.WriteLine("Hello My Name Is Tran Manh Tung");
         }
     }
 }
